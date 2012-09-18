30825ccb416468edd5645146f03058c448789ea4
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2012-09-18 18:22:37 +0400

    Some text improvements

--- Controller/ImagePlaceholderController.php
+++ Controller/ImagePlaceholderController.php
@@ -16,24 +16,28 @@ class ImagePlaceholderController extends Controller
         $query = $this->getRequest()->query;
         $y = $y ? : $x;
 
-        $text = $query->get('text');
-        $text = strlen($text) > 0 ? $text : sprintf('%dx%d', $x, $y);
-        $textLength = strlen($text);
-
-        $fontFile = __DIR__ . '/../Resources/fonts/Orienta-Regular.ttf';
-        $fontSize = $textLength > 4 ? $x / $textLength : $x / 4;
-
-        $bbox = imagettfbbox($fontSize, 0, $fontFile, $text);
-        $textX = ceil(($x - $bbox[2] - $bbox[0]) / 2);
-        $textY = ceil(($y - $bbox[7] - $bbox[1]) / 2);
-
         $image = imagecreatetruecolor($x, $y);
 
         $backgroundColor = $this->allocateColor($image, $backgroundColor ? : 'babdb6');
-        $textColor = $this->allocateColor($image, $textColor ? : '2e3436');
-
         imagefilledrectangle($image, 0, 0, $x - 1, $y - 1, $backgroundColor);
-        imagefttext($image, $fontSize, 0, $textX, $textY, $textColor, $fontFile, $text);
+
+        if ($x >= 20) {
+            $text = $query->get('text');
+            $text = strlen($text) > 0 ? $text : sprintf('%dx%d', $x, $y);
+            $textLength = strlen($text);
+            $textColor = $this->allocateColor($image, $textColor ? : '2e3436');
+
+            $fontFile = __DIR__ . '/../Resources/fonts/Orienta-Regular.ttf';
+            $maxFontSize = $x < 50 ? $x / 3 : $x / 4;
+            $fontSize = $x / $textLength;
+            $fontSize = $fontSize > $maxFontSize ? $maxFontSize : $fontSize;
+
+            $bbox = imagettfbbox($fontSize, 0, $fontFile, $text);
+            $textX = ceil(($x - $bbox[2] - $bbox[0]) / 2);
+            $textY = floor(($y - $bbox[7] - $bbox[1]) / 2);
+
+            imagefttext($image, $fontSize, 0, $textX, $textY, $textColor, $fontFile, $text);
+        }
 
         $draw = function () use ($image) {
             imagegif($image);
